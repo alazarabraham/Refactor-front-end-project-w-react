1ef1a4496571aff0f9253cd464671d9537a3e83f
alazarabraham <54673208+alazarabraham@users.noreply.github.com>
2019-11-07 13:29:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router,Route, Link} from "react-router-dom";
+import Wrapper from "./components/wrapper";
+// import CategoryList from "./components/search";
 import './App.css';
+import "bulma/css/bulma.css";
+import {Button, MenuList} from "bloomer";
+import Ingredient from './components/ingredient';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <h1>Recipe Junkie</h1>
+          </header>
+          <Wrapper>
+              <MenuList>
+                  <Ingredient/>
+              </MenuList>
+        </Wrapper>
+      </div>
   );
 }
 
